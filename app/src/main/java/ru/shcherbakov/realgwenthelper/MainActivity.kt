c9c131dee25eda2f01c2f4a4e5e6f3e7cf311309
Arantier <shcherbakov_dmitry@bk.ru>
2020-02-02 15:49:10 +0300

    Replaced horn button with two effect buttons - horn and scorch Made rows and decks reusable layouts

--- app/src/main/java/ru/shcherbakov/realgwenthelper/MainActivity.kt
+++ app/src/main/java/ru/shcherbakov/realgwenthelper/MainActivity.kt
@@ -2,11 +2,16 @@ package ru.shcherbakov.realgwenthelper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.include_deck.view.*
+import kotlinx.android.synthetic.main.include_row.view.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        secondPlayerDeck.rotation = 180.0f
     }
 }
